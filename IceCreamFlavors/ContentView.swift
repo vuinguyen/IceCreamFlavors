144b1995a1b20dcfb1b4b2b62414ca0ddaddcc36
Vui Nguyen <vui_engineer@yahoo.com>
2020-08-23 16:06:14 -0600

    Initial Commit

---
+++ IceCreamFlavors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IceCreamFlavors
+//
+//  Created by Vui Nguyen on 8/23/20.
+//  Copyright © 2020 SunfishEmpire. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
