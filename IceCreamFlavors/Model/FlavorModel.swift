d26ee15164c1c60e9a00de0444a2150eba228ee8
Vui Nguyen <vui_engineer@yahoo.com>
2020-08-30 08:35:59 -0600

    add rating to FlavorModel

--- IceCreamFlavors/Model/FlavorModel.swift
+++ IceCreamFlavors/Model/FlavorModel.swift
@@ -14,21 +14,22 @@ struct FlavorItem: Identifiable {
     var id: Int
     var name: String
     var description: String
+    var rating: Int
 }
 
-let flavorOfMonthItem = FlavorItem(id: 4, name: "Triple Mango", description: "Mango ice cream with real mango pieces, swirled with a rich mango sorbet and finished with a sweet mango ribbon.")
+let flavorOfMonthItem = FlavorItem(id: 4, name: "Triple Mango", description: "Mango ice cream with real mango pieces, swirled with a rich mango sorbet and finished with a sweet mango ribbon.", rating: 5)
 
 struct FlavorModel {
     var flavors:[FlavorItem] = [
-        FlavorItem(id: 0, name: "Mom's Makin' Cookies", description: "Mom's secret recipe of brown sugar flavored ice cream filled with chocolate chip cookie pieces, chocolate flavored chips, and a delicious cookie dough batter flavored ribbon."),
-        FlavorItem(id: 1, name: "Surprise Party", description: "A colorful blend of blue and pink cake batter flavored ice cream with yellow buttercream flavored ice cream."),
-        FlavorItem(id: 2, name: "Cotton Candy Crackle", description: "Cotton candy flavored ice cream swirled with a cotton candy flavored ribbon and yellow poppin' candy pieces."),
-        FlavorItem(id: 3, name: "Raspberry Sorbet", description: "Delicious raspberries from the Pacific Coast make this sorbet light and refreshing."),
-        FlavorItem(id: 4, name: "Triple Mango", description: "Mango ice cream with real mango pieces, swirled with a rich mango sorbet and finished with a sweet mango ribbon."),
-        FlavorItem(id: 5, name: "Non-Dairy & Vegan Chocolate Chip Cookie Dough", description: "Try this rich, indulgent cookie dough flavor with cookie dough pieces, chocolate chips, and a chocolate fudge swirl. By the way, it's all Non-Dairy and Vegan."),
-        FlavorItem(id: 6, name: "Baseball Nut", description: "Here's the pitch: vanilla flavored ice cream and cashews with a black raspberry ribbon. Catch it while you can."),
-        FlavorItem(id: 7, name: "No Sugar Added Caramel Turtle Truffle Ice Cream", description: "Reduced fat, no sugar added vanilla flavored ice cream packed with no sugar added caramel-filled milk chocolate flavored turtles and a caramel swirl."),
-        FlavorItem(id: 8, name: "Cherries Jubilee", description: "You're invited to a jubilee! Other guests include cherry flavored ice cream, cherry halves, and a hint of rum flavor."),
-        FlavorItem(id: 9, name: "Chocolate Almond", description: "We haven't gone nuts. But our chocolate ice cream loaded with roasted almonds? Well, that's another matter.")
+        FlavorItem(id: 0, name: "Mom's Makin' Cookies", description: "Mom's secret recipe of brown sugar flavored ice cream filled with chocolate chip cookie pieces, chocolate flavored chips, and a delicious cookie dough batter flavored ribbon.", rating: 4),
+        FlavorItem(id: 1, name: "Surprise Party", description: "A colorful blend of blue and pink cake batter flavored ice cream with yellow buttercream flavored ice cream.", rating: 3),
+        FlavorItem(id: 2, name: "Cotton Candy Crackle", description: "Cotton candy flavored ice cream swirled with a cotton candy flavored ribbon and yellow poppin' candy pieces.", rating: 3),
+        FlavorItem(id: 3, name: "Raspberry Sorbet", description: "Delicious raspberries from the Pacific Coast make this sorbet light and refreshing.", rating: 5),
+        FlavorItem(id: 4, name: "Triple Mango", description: "Mango ice cream with real mango pieces, swirled with a rich mango sorbet and finished with a sweet mango ribbon.", rating: 5),
+        FlavorItem(id: 5, name: "Non-Dairy & Vegan Chocolate Chip Cookie Dough", description: "Try this rich, indulgent cookie dough flavor with cookie dough pieces, chocolate chips, and a chocolate fudge swirl. By the way, it's all Non-Dairy and Vegan.", rating: 4),
+        FlavorItem(id: 6, name: "Baseball Nut", description: "Here's the pitch: vanilla flavored ice cream and cashews with a black raspberry ribbon. Catch it while you can.", rating: 4),
+        FlavorItem(id: 7, name: "No Sugar Added Caramel Turtle Truffle Ice Cream", description: "Reduced fat, no sugar added vanilla flavored ice cream packed with no sugar added caramel-filled milk chocolate flavored turtles and a caramel swirl.", rating: 5),
+        FlavorItem(id: 8, name: "Cherries Jubilee", description: "You're invited to a jubilee! Other guests include cherry flavored ice cream, cherry halves, and a hint of rum flavor.", rating: 5),
+        FlavorItem(id: 9, name: "Chocolate Almond", description: "We haven't gone nuts. But our chocolate ice cream loaded with roasted almonds? Well, that's another matter.", rating: 3)
     ]
 }
